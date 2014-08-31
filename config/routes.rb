e659f96358b018bb12bbbaed19fde5eb298df3dc
Franco Cedillo <franco.cedillo@gmail.com>
2014-08-30 19:05:02 -0500

    Include weeks in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,8 @@ Rails.application.routes.draw do
   #     resource :seller
   #   end
 
+resources :weeks
+
   # Example resource route with more complex sub-resources:
   #   resources :products do
   #     resources :comments
