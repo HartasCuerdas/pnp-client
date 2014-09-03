67e928866cd6a1ef4603e4d846f5ec716301541c
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-03 12:20:13 -0500

    adds #index, #show, #detroy

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,8 +1,28 @@
 class WeeksController < ApplicationController
+  before_action :set_week, only: [:show, :destroy]
 
   # GET /weeks
   def index
     @weeks = Week.all
+
+    dateToday = Date.today
+    @strToday = dateToday.strftime('%b %d, %a')
   end
   
+  # GET /weeks/1
+  def show
+  end
+
+  # DELETE /weeks/1
+  def destroy
+    @week.destroy
+    redirect_to weeks_url, notice: 'Week was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_week
+      @week = Week.find(params[:id])
+    end
+
 end
