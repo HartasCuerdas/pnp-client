c7db9658c5b13edb5d359b69fc17715d11be3c74
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-03 12:20:39 -0500

    parsing methods

--- app/models/week.rb
+++ app/models/week.rb
@@ -1,3 +1,19 @@
 class Week < ActiveResource::Base
   self.site = "http://localhost:3000"
+
+  def str_firstDay
+    date_firstDay = self.firstDay.to_date
+    date_firstDay.strftime('%b %e')
+  end
+
+  def str_oAVG
+    float_firstDay = self.oAVG.to_f
+    float_firstDay.round(1)
+  end
+
+  def str_dAVG
+    float_firstDay = self.dAVG.to_f
+    float_firstDay.round(1)
+  end
+
 end
