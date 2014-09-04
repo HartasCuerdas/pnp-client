9d84575381427bf91dcee5df0a7be321001155fe
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-04 00:52:56 -0500

    toggle_d action

--- app/controllers/ods_controller.rb
+++ app/controllers/ods_controller.rb
@@ -1,9 +1,15 @@
 class OdsController < ApplicationController
-  before_action :set_od, only: [:toggle_o]
+  before_action :set_od, only: [:toggle_o, :toggle_d]
   
   # TOGGLE_O /ods/1
   def toggle_o
     res = Od.put("#{@od.id}/toggle_o")
+    render :nothing => true
+  end
+
+  # TOGGLE_D /ods/1
+  def toggle_d
+    res = Od.put("#{@od.id}/toggle_d")
     render :nothing => true
   end
 
