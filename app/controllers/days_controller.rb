28c8e7dc10371ca13efaa330cd25229daff3b5c7
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-04 16:21:21 -0500

    toggle_wr action

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -6,6 +6,12 @@ class DaysController < ApplicationController
     @day.ods = Od.find(:all, :from => "/days/#{@day.id}/ods.json")
   end
 
+  # TOGGLE_WR /days/1
+  def toggle_wr
+    Day.put("#{params[:id]}/toggle_wr")
+    render :nothing => true
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_day
