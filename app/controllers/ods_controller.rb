8178eda4e3de5e43cb264a28f2ce98ba876d88fc
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-04 13:24:11 -0500

    optimized ods

--- app/controllers/ods_controller.rb
+++ app/controllers/ods_controller.rb
@@ -1,22 +1,15 @@
 class OdsController < ApplicationController
-  before_action :set_od, only: [:toggle_o, :toggle_d]
   
   # TOGGLE_O /ods/1
   def toggle_o
-    res = Od.put("#{@od.id}/toggle_o")
+    Od.put("#{params[:id]}/toggle_o")
     render :nothing => true
   end
 
   # TOGGLE_D /ods/1
   def toggle_d
-    res = Od.put("#{@od.id}/toggle_d")
+    Od.put("#{params[:id]}/toggle_d")
     render :nothing => true
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_od
-      @od = Od.find(params[:id])
-    end
 
 end
