6183675aa6a06978138f2be06bc61a4e2cda8a87
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-24 21:26:26 -0500

    adds some documentation

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,27 +1,36 @@
 class Day < ResourceBase
 
+  # Text value for boolean
   WR_TEXT_TRUE = I18n.t('Well')
+  # Text value for boolean
   WR_TEXT_FALSE = I18n.t('Poor')
   
   # Twitter Bootstrap Button Style Class
   WR_STYLE_TRUE = 'btn-success'
+  # Twitter Bootstrap Button Style Class
   WR_STYLE_FALSE = 'btn-danger'
 
+  # Style class
   IS_TODAY_STYLE = 'pnpC-is-today'
 
+  # returns format date string for date
   def str_date
     date_date = date.to_date
     I18n.l(date_date, format: :m_d_wd)
   end
 
+  # returns string for well_registered boolean
   def str_wr
     well_registered ? WR_TEXT_TRUE : WR_TEXT_FALSE
   end
 
+  # returns Style class for well_registered boolean
   def str_wr_TwbsBtnStyleClass
     well_registered ? WR_STYLE_TRUE : WR_STYLE_FALSE
   end
 
+  # returns Style for current day
+  # used in Week show (table of days)
   def str_isTodayClass
     (date == Date.today.to_s) ? IS_TODAY_STYLE : ''
   end
--- app/models/week.rb
+++ app/models/week.rb
@@ -3,16 +3,19 @@ class Week < ResourceBase
   # Style class for current week
   IS_CURRENT_WEEK_STYLE = 'pnpC-is-current-week'
 
+  # returns localized date for firstDay string
   def str_firstDay
     date_firstDay = firstDay.to_date
     I18n.l(date_firstDay, format: :m_d)
   end
 
+  # returns rounded float for oAVG string
   def str_oAVG
     float_firstDay = oAVG.to_f
     float_firstDay.round(1)
   end
 
+  # returns rounded float for dAVG string
   def str_dAVG
     float_firstDay = dAVG.to_f
     float_firstDay.round(1)
