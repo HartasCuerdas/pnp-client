7e21ad7add7eeb3fa742a9153851ca5d85d6eb79
Franco Cedillo <franco.cedillo@gmail.com>
2014-09-24 20:57:07 -0500

    adds logic for str_isCurrentWeekClass

--- app/models/week.rb
+++ app/models/week.rb
@@ -1,5 +1,8 @@
 class Week < ResourceBase
 
+  # Style class for current week
+  IS_CURRENT_WEEK_STYLE = 'pnpC-is-current-week'
+
   def str_firstDay
     date_firstDay = firstDay.to_date
     I18n.l(date_firstDay, format: :m_d)
@@ -13,6 +16,13 @@ class Week < ResourceBase
   def str_dAVG
     float_firstDay = dAVG.to_f
     float_firstDay.round(1)
+  end
+
+  # returns IS_CURRENT_WEEK_STYLE for current week  
+  # used in Weeks index
+  def str_isCurrentWeekClass
+    currentWeekFirstDay = Date.today.beginning_of_week(:sunday)
+    (firstDay.to_s == currentWeekFirstDay.to_s) ? IS_CURRENT_WEEK_STYLE : ''
   end
 
 end
